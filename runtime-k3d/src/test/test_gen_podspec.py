3c3968cacee7d1fc75caa6249abfafcd11817b4b
Markus Petke <MP91@users.noreply.github.com>
2023-05-05 12:30:02 +0200

    initial test

--- runtime-k3d/src/test/test_gen_podspec.py
+++ runtime-k3d/src/test/test_gen_podspec.py
@@ -11,8 +11,33 @@
 # under the License.
 #
 # SPDX-License-Identifier: Apache-2.0
+import os
+import sys
+
+sys.path.append(os.path.abspath("/workspaces/devenv-runtimes/runtime-k3d/src"))
+from gen_podspec import generate_port_spec
+from lib import ServiceSpecConfig
 
 
-def test_abc():
-    test_var = "abc"
-    assert test_var == "abc"
+def test_gen_port_spec():
+    port_spec = generate_port_spec(
+        ServiceSpecConfig(None, None, None, None, ["1234", "5678", "0"], None)
+    )
+    desired = [
+        {
+            "name": "port1234",
+            "containerPort": 1234,
+            "protocol": "TCP",
+        },
+        {
+            "name": "port5678",
+            "containerPort": 5678,
+            "protocol": "TCP",
+        },
+        {
+            "name": "port0",
+            "containerPort": 0,
+            "protocol": "TCP",
+        },
+    ]
+    assert port_spec == desired
